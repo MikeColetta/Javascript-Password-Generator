43985e185c7f526d4ca9c255379eeb6111925ae8
Mike Coletta <coletta.mike@gmail.com>
2021-01-06 14:31:14 -0800

    Created character variables.

---
+++ Develop/script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"]
+var numericChar = [0,1,2,3,4,5,6,7,8,9,]
+var lowercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var uppercaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+console.log(specialChar)
+console.log(numericChar)
+console.log(lowercaseChar)
+console.log(uppercaseChar)
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
