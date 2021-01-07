cf880abafb5cc4b1c7dcba9e4654f31fcb77e123
Mike Coletta <coletta.mike@gmail.com>
2021-01-06 18:22:49 -0800

    Added prompts.

--- Develop/script.js
+++ Develop/script.js
@@ -3,22 +3,49 @@ var generateBtn = document.querySelector("#generate");
 
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"]
 var numericChar = [0,1,2,3,4,5,6,7,8,9,]
-var lowercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var uppercaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var allChosenChar = []
+
+//can I make an array without typing every number 1-128 out?
+const passwordMaxLength = 128
+const passwordMinLength = 8
 
 
-console.log(specialChar)
-console.log(numericChar)
-console.log(lowercaseChar)
-console.log(uppercaseChar)
+// console.log(specialChar)
+// console.log(numericChar)
+// console.log(lowercaseChar)
+// console.log(uppercaseChar)
 
-//select criteria
+//select criteria using ids from the html
 
-//ask password length
-
+//ask password length function
+ 
 //password criteria (Lowercase? Uppercase? Numbers? Special Characters?)
 
 //validate character input
+function generatePassword () {
+  var passwordLength = prompt("How long is your password?")
+
+  if (passwordLength<passwordMinLength || passwordLength>passwordMaxLength) {
+    alert("Password is not between 8 - 128 characters!")
+    return ""
+  }
+
+  var hasLowercase = confirm("Do you want lowercase?")
+  var hasUppercase = confirm("Do you want uppercase?")
+  var hasNumbers = confirm("Do you want numbers?")
+  var hasSpecChar = confirm("Do you want special characters?")
+  var password = ""
+
+  for(var i=0; i<passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random()*lowerCaseChar.length)
+    password+=lowerCaseChar[randomIndex]
+    }
+  return password
+}
+
+Math.random
 
 
 // Write password to the #password input
