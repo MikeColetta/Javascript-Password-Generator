5058a3597f889d8e9e92c48646e3ce81e9edde52
Mike Coletta <coletta.mike@gmail.com>
2021-01-08 14:24:15 -0800

    Cleaned up code.

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//character arrays
+//Character arrays
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"]
 var numericChar = [0,1,2,3,4,5,6,7,8,9,]
 var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var allChosenChar = []
 
-//password lengths
+//Password lengths
 const passwordMaxLength = 128
 const passwordMinLength = 8
 
-// console.log(allChosenChar.concat(specialChar, numericChar, upperCaseChar, lowerCaseChar))
+//Gets password criteria.
 
-// console.log(specialChar)
-// console.log(numericChar)
-// console.log(lowercaseChar)
-// console.log(uppercaseChar)
-
-//gets password criteria.
-
-//gets password length
+//Gets password length
 function generatePassword () {
   var passwordLength = prompt("How long is your password?");
 
-  //if password is not between 8 - 128 characters
+  //If password is not between 8 - 128 characters
   if (passwordLength<passwordMinLength || passwordLength>passwordMaxLength) {
     alert("Password is not between 8 - 128 characters!");
     return ""
   }
   
-  //asks and adds lowercase if true
+  //Asks and adds lowercase if true
   var hasLowercase = confirm("Do you want lowercase?");
   if (hasLowercase = true)  {
     allChosenChar += lowerCaseChar;
   }
   
-  //asks and adds uppercase if true
+  //Asks and adds uppercase if true
   var hasUppercase = confirm("Do you want uppercase?");
   if (hasUppercase = true) {
     allChosenChar += upperCaseChar;
   }
   
-  //asks and adds numbers if true
+  //Asks and adds numbers if true
   var hasNumbers = confirm("Do you want numbers?");
   if (hasNumbers = true) {
     allChosenChar += numericChar;
   }
 
-  //asks and adds special characters if true
+  //Asks and adds special characters if true
   var hasSpecChar = confirm("Do you want special characters?");
   if (hasSpecChar = true) {
     allChosenChar += specialChar;
@@ -57,7 +50,7 @@ function generatePassword () {
   
   var password = ""
 
-  //randomizes passoword based on character criteria
+  //Randomizes password based on character criteria
   for(var i=0; i<passwordLength; i++) {
     var randomIndex = Math.floor(Math.random()*allChosenChar.length);
     password+=allChosenChar[randomIndex]
