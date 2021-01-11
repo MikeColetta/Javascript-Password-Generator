d6a25402af98d6ea3e3400d84f54ff903454280e
Mike Coletta <coletta.mike@gmail.com>
2021-01-10 17:55:44 -0800

    Fixed commas in password issue.

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 //Character arrays
-var specialChar = [" !", "@", "#", "$", "%", "^", "&", "*"]
-var numericChar = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var upperCaseChar = [" A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var allChosenChar = ""
+var specialChar = ['!@#$%^&']
+var numericChar = ['0123456789']
+var upperCaseChar = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+var lowerCaseChar = ['abcdefghijklmnopqrstuvwxyz']
+var allChosenChar = [""]
 
 //ADD CONSOLE LOGS
 
